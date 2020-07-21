185064bb17ce996f11591010236c065c71403dd3
Roberto Perez Soriano <robertops@Robertos-MacBook-Pro.local>
2020-07-21 13:52:12 -0700

    Updates

---
+++ precache-manifest.141991809f214f372f7b19d2a128bc0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb4082acc45e39a83b565deffaf934c7",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b318c3220e90d755d36a",
+    "url": "/portfolio/static/css/main.7f324d9b.chunk.css"
+  },
+  {
+    "revision": "eb9277052ad5ce4e686f",
+    "url": "/portfolio/static/js/2.876be4a5.chunk.js"
+  },
+  {
+    "revision": "0f544458a968df1596e861027de46f92",
+    "url": "/portfolio/static/js/2.876be4a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b318c3220e90d755d36a",
+    "url": "/portfolio/static/js/main.93b9a23a.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
